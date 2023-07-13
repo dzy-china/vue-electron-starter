5560fe11bd8bc286aa420fd888af59845bda2350
dzy <1129881228@qq.com>
2023-07-14 02:25:46 +0800

    细节优化

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,5 @@ import  { contextBridge ,ipcRenderer}   from 'electron'
 
 // 暴露给渲染进程的数据，通过：window.main获取
 contextBridge.exposeInMainWorld('main',{
-    ipcRenderer: {...ipcRenderer},
-    expressPort: process.env.expressPort,
+    ipcRenderer: {...ipcRenderer}
 })
